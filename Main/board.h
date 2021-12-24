9da20efea61d599d83a6855242d07ddd7eb7a0a7
FXYFXZ <fxy@mail.ru>
2021-12-24 16:57:41 +0300

    Procs

--- Main/board.h
+++ Main/board.h
@@ -6,37 +6,37 @@
 //-------------------------------------
 
 //  PORTB
-#define dirb (0<<PB7)+(0<<PB6)+(0<<PB5)+(0<<PB4)+(0<<PB3)+(1<<PB2)+(0<<PB1)+(1<<PB0) // 1-out, 0-in
-#define opb  (0<<PB7)+(0<<PB6)+(0<<PB5)+(0<<PB4)+(0<<PB3)+(1<<PB2)+(0<<PB1)+(0<<PB0) // 1-pull up on, 0-Z
-#define PB_LED3   PB0  // O:
-#define PB_LED2   PB1  // O:
-#define PB_LED1   PB2  // O:
-#define PB_LCD_A0 PB3  // O: LCD_A0
-#define PB_LCDE   PB4  // O:
-#define PB_LCDRW  PB5  // O:
+#define dirb (0<<PB7)+(0<<PB6)+(0<<PB5)+(1<<PB4)+(0<<PB3)+(1<<PB2)+(1<<PB1)+(1<<PB0) // 1-out, 0-in
+#define opb  (0<<PB7)+(0<<PB6)+(0<<PB5)+(0<<PB4)+(0<<PB3)+(0<<PB2)+(0<<PB1)+(0<<PB0) // 1-pull up on, 0-Z
+#define PB_LEDG2  PB0  // O: LED Green 2
+#define PB_485TXE PB1  // O:
+#define PB_LEDR4  PB2  // O: LED Red 4
+#define PB_BUZZ   PB3  // O:
+#define PB_LEDY1  PB4  // O:
+#define PB_5      PB5  // O:
 #define PB_6      PB6  // NC- Osc
 #define PB_7      PB7  // NC- Osc
 
 //  PORTC
 #define dirc (0<<PC6)+(0<<PC5)+(0<<PC4)+(0<<PC3)+(0<<PC2)+(0<<PC1)+(0<<PC0) // 1-out, 0-in
-#define opc  (0<<PC6)+(0<<PC5)+(0<<PC4)+(0<<PC3)+(0<<PC2)+(0<<PC1)+(0<<PC0) // 1-pull up on, 0-Z
-#define PC_LCD_DD4  PC0       // O: LCD DATA
-#define PC_LCD_DD5  PC1       // O:
-#define PC_LCD_DD6  PC2       // O:
-#define PC_LCD_DD7  PC3       // O:
-#define PC_SDA      PC4       // I:
-#define PC_SCL      PC5       // I:
+#define opc  (0<<PC6)+(1<<PC5)+(1<<PC4)+(1<<PC3)+(1<<PC2)+(1<<PC1)+(0<<PC0) // 1-pull up on, 0-Z
+#define PC_0        PC0       // O:
+#define PC_J1       PC1       // O:
+#define PC_J2       PC2       // O:
+#define PC_J3       PC3       // O:
+#define PC_SDA      PC4       // I:  J4
+#define PC_SCL      PC5       // I:  J5
 #define PC_RESET    PC6       // I: RESET
 
 // PORTD
-#define dird (1<<PD7)+(0<<PD6)+(0<<PD5)+(1<<PD4)+(1<<PD3)+(0<<PD2)+(0<<PD1)+(0<<PD0) // 1-out, 0-in
-#define opd  (1<<PD7)+(0<<PD6)+(0<<PD5)+(0<<PD4)+(1<<PD3)+(0<<PD2)+(0<<PD1)+(0<<PD0) // 1-pull up on, 0-Z
-#define PD_KEYB     PD0       // I: Вход последовательного порта
-#define PD_TXD      PD1       // O: Выход последовательного порта
-#define PD_LCD_ON   PD2       // O: Rs-485-TX Enable
-#define PD_KEYA     PD3       // O:
-#define PD_KEYC     PD4       // I:
-#define PD_BUZZ     PD5       // O: Бузер
-#define PD_LCDBL    PD6       // O: Подсветка LCD
-#define PD_LED4     PD7       // X:
+#define dird (1<<PD7)+(1<<PD6)+(0<<PD5)+(1<<PD4)+(1<<PD3)+(1<<PD2)+(0<<PD1)+(0<<PD0) // 1-out, 0-in
+#define opd  (0<<PD7)+(0<<PD6)+(1<<PD5)+(0<<PD4)+(0<<PD3)+(0<<PD2)+(0<<PD1)+(0<<PD0) // 1-pull up on, 0-Z
+#define PD_0        PD0       // I:
+#define PD_1        PD1       // O:
+#define PD_LEDR1    PD2       // O:
+#define PD_LEDR5    PD3       // O:
+#define PD_LEDR2    PD4       // I:
+#define PD_TM       PD5       // O: TM
+#define PD_LEDG1    PD6       // O:
+#define PD_LEDR3    PD7       // X:
 
--- Main/main.cpp
+++ Main/main.cpp
@@ -3,13 +3,49 @@
 
 /* Protos */
 void LowLevelInit();
+void SecondProc();
+void Proc50ms();
 
-
+/* Locals */
+bool gl10ms = false;
+#define TIMER_VALUE 0xFFFF-97
 
 int main() {
+  static U8 cnt50ms = 5;
+  static U8 cntSec = 20;
   LowLevelInit();
-  while(true);
+
+    // Main Loop
+    while(true) {
+        if (gl10ms){ // 10 ms
+            gl10ms = false;
+            if (--cnt50ms==0){  // 50 ms
+                cnt50ms = 5;
+                Proc50ms();
+
+
+                 if (--cntSec==0){  // 1s
+                    cntSec = 20;
+                    SecondProc();
+
+                 }
+            }
+        }
+    }
 }
+
+
+void Proc50ms(){
+    BIT_XOR(PORTD, PD_LEDR1);
+
+}
+
+
+void SecondProc(){
+    BIT_XOR(PORTD, PD_LEDR5);
+}
+
+
 
 
 void Port_Setup (void){
@@ -34,7 +70,21 @@ void LowLevelInit(){
     EEAR = 0x00; // ”становка регистра адреса EEPROM на нулевую €чейку
     EECR = 0x00;
 
+    // Timers setup
+    TCCR1B = (1<<CS12)+(0<<CS11)+(1<<CS10);  //
+    TCNT1 = TIMER_VALUE;                     //
+    TIMSK1 |= (1<<TOIE1);
+
+
     __enable_interrupt();
+}
+
+
+// прерывание системного таймера 10 ms
+#pragma vector = TIMER1_OVF_vect
+__interrupt void TIMER1_OVF_interrupt(void) {
+    TCNT1 = TIMER_VALUE; // load for next interval
+    gl10ms = true;
 }
 
 
